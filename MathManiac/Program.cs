d85273a0c5b96361dae041751c09558469b20582
hejmdahl <moahjemdahl@outlook.com>
2020-12-29 12:30:54 +0100

    testtest

--- MathManiac/Program.cs
+++ MathManiac/Program.cs
@@ -6,7 +6,7 @@ namespace MathManiac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hellooo");
         }
     }
 }
