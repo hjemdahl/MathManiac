c305cf2f66e59a190c8cb0f0782717c4e65b535d
hejmdahl <moahjemdahl@outlook.com>
2020-12-29 12:30:17 +0100

    * .gitignore: test
    * MathManiac.sln:
    * Program.cs:
    * MathManiac.csproj:

---
+++ MathManiac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathManiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
